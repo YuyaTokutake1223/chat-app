93b9db0e3b00eaa955d9d734b5bb4e2d9fee4007
YuyaTokutake1223 <yuya-tokutake@a-hub.net>
2024-09-06 10:34:42 +0900

    room_usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,7 @@ class User < ApplicationRecord
   validates :password,           presence: true
   validates :encrypted_password, presence: true
 
+  has_many :room_users
+  has_many :rooms, through: :room_users
 
 end
